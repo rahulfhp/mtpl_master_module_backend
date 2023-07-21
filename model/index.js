fdb5c04d70bf7bcccd6723c556cec4d9d33286b6
Yash Bajaj <yash.bajaj56@gmail.com>
2023-07-21 17:23:57 +0530

    Added timetable relation

--- model/index.js
+++ model/index.js
@@ -2,6 +2,12 @@ const sequelize = require("../config/database");
 const User = require("./User");
 const OtpVerification = require("./OtpVerification");
 const ProfessionalDetails = require("./ProfessionalDetails");
+const TimeTable = require('./TimeTableModel')
+
+TimeTable.belongsTo(User, {
+  foreignKey: 'user_id', // This will create a foreign key named 'userId' in the Time table
+  allowNull: false, // Make the association required (not null)
+});
 
 const connectToDb = async () => {
   try {
