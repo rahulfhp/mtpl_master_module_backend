ac53906e9d80afebdd0578b71f8fa084ba2cd2c4
Yash Bajaj <yash.bajaj56@gmail.com>
2023-07-21 10:37:56 +0530

    CHanges in Login Controller

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,51 +1,92 @@
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const emailValidator = require("email-validator");
+const { generateToken } = require("../middleware/generateToken");
+require("dotenv").config();
 const {
   emailPasswordError,
   noUser,
   passwordError,
   loggedIn,
+  errorFunc,
 } = require("../constraints/errorMessage");
 
 const login = async (req, res) => {
-  let { email, password } = req.body;
+  let { loginId, password } = req.body;
+
   try {
-    if (!email || !password) {
-      return res.send({
-        status: 400,
-        message: emailPasswordError,
-      });
-    } else {
-      const user = await User.findOne({
+    if (loginId ==="" || password==="") {
+      return errorFunc(res, 400, emailPasswordError);
+    }
+
+    let isEmail = emailValidator.validate(loginId);
+    console.log('email--',isEmail);
+    let user = null;
+    if (isEmail) {
+      user = await User.findOne({
         where: {
-          email,
+          email: loginId,
         },
       });
-      // console.log(user.dataValues.password);
       if (!user) {
-        return res.send({
-          status: 400,
-          message: noUser,
-        });
-      } else {
-        const pass = await bcrypt.compare(password, user.dataValues.password);
-        if (!pass) {
-          return res.send({
-            status: 400,
-            message: passwordError,
-          });
-        }
-        return res.send({
-          status: 200,
-          message: loggedIn,
-        });
+        return errorFunc(res,400,noUser)
+      }
+      if (user.dataValues.is_verfied) {
+        return errorFunc(res,400,"Your Email is not verfied Yet")
       }
     }
-  } catch (error) {
-    return res.send({
-      status: 400,
-      message: error.message,
+    else{
+    user = await User.findOne({
+      where: {
+        phone: loginId,
+      },
     });
+    if (!user) {
+      return errorFunc(res,400,noUser)
+    }
+    if (user.dataValues.is_verfied) {
+      return errorFunc(res,400,"Your Phone Number is not verfied Yet")
+    }
+  }
+    // console.log(user.dataValues.password);
+    console.log("user22----",user);
+    if (!user) {
+      return errorFunc(res, 400, noUser);
+    } else {
+      let pass = await bcrypt.compare(password, user.dataValues.password);
+      if (!pass) {
+        return errorFunc(res, 400, passwordError);
+      }
+
+      const token = await generateToken(req, res, user.dataValues.user_id);
+      let userUpdate = null;
+      if (isEmail) {
+        userUpdate = await User.update(
+          { token },
+          {
+            where: {
+              email: loginId,
+            },
+          }
+        );
+      }
+      userUpdate = await User.update(
+        { token },
+        {
+          where: {
+            phone: loginId,
+          },
+        }
+      );
+
+      if (!userUpdate) {
+        return errorFunc(res, 400, "SomeThing Went Wrong");
+      }
+      return errorFunc(res, 200, loggedIn);
+    }
+  } catch (error) {
+    return errorFunc(res, 400, error.message);
   }
 };
 
