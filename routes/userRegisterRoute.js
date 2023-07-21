7bbbe98ceb5d898d735f2215aecb2925f0bffd45
Yash Bajaj <yash.bajaj56@gmail.com>
2023-07-21 10:33:36 +0530

    Changes in User Register Route

--- routes/userRegisterRoute.js
+++ routes/userRegisterRoute.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const { changePassword } = require("../controllers/changePasswordController");
 const { login } = require("../controllers/loginController");
-const { resetPassword } = require("../controllers/resetPasswordController");
+const { newPassword } = require("../controllers/newPassword");
+const { verificationOTP } = require("../controllers/otpVerification");
+const { emailVerification } = require("../controllers/emailVerificationController");
+const { updateProfile } = require("../controllers/updateProfileController");
 const { userRegister } = require("../controllers/userRegisterController");
+const {authVerification} = require('../middleware/tokenAuthorization');
+const { getProfileInfo } = require("../controllers/getProfileInfo");
+
 
 
 const router = express.Router();
 
 router.post("/register", userRegister);
-router.post("/login", login);
-router.post("/resetpassword", resetPassword);
-router.post("/changepassword",changePassword)
+router.post("/login",login);
+router.post("/emailverification", emailVerification);
+router.post("/changepassword/:userid",authVerification,changePassword)
+router.post("/updateprofile/:userid",authVerification,updateProfile)
+router.post('/otpverification/:loginId/:type',verificationOTP)
+router.post('/newpassword/:loginId/:otp',newPassword)
+router.get('/getprofileinfo/:userid',authVerification,getProfileInfo)
 module.exports = router;
